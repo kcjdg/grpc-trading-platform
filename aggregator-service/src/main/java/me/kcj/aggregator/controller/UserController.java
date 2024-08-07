c698811dd36cd2529eedc84f5d5ff6470323a6a7
kcjdg <kencrisjohn@gmail.com>
2024-08-07 15:37:14 +0800

    Grpc client cannot found spring DI using constructor injection, replace by using @Autowired

--- aggregator-service/src/main/java/me/kcj/aggregator/controller/UserController.java
+++ aggregator-service/src/main/java/me/kcj/aggregator/controller/UserController.java
@@ -3,6 +3,7 @@ package me.kcj.aggregator.controller;
 import lombok.RequiredArgsConstructor;
 import me.kcj.aggregator.service.UserService;
 import me.kcj.user.UserInformation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
-@RequiredArgsConstructor
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
