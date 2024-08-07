bb3403d5d2a9ea1fa80a653c15a29e9dcc9b75e6
kcjdg <kencrisjohn@gmail.com>
2024-08-07 15:36:00 +0800

    ExceptionHandler for StatusRuntimeException

--- aggregator-service/src/main/java/me/kcj/aggregator/controller/advice/ApplicationExceptionHandler.java
+++ aggregator-service/src/main/java/me/kcj/aggregator/controller/advice/ApplicationExceptionHandler.java
@@ -3,10 +3,13 @@ package me.kcj.aggregator.controller.advice;
 import io.grpc.StatusRuntimeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
+
+    @ExceptionHandler(StatusRuntimeException.class)
     public ResponseEntity<String> handleStatusRuntimeException(StatusRuntimeException e){
         return switch (e.getStatus().getCode()){
             case INVALID_ARGUMENT, FAILED_PRECONDITION -> ResponseEntity.badRequest().body(e.getStatus().getDescription());
